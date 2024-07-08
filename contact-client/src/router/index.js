5702f93a0088d28f004957867c0149a06281854b
Kafayat <kafayat@onepipe.io>
2024-07-08 14:15:37 +0100

    install axio and set routing

--- contact-client/src/router/index.js
+++ contact-client/src/router/index.js
@@ -1,23 +1,65 @@
+import LoginPage from '@/views/LoginPage.vue'
+import ProfilePage from '@/views/ProfilePage.vue'
+import SignUpPage from '@/views/SignUpPage.vue'
+import EmailVerify from "@/views/EmailVerify.vue"
+import ContactList from "@/views/ContactList.vue"
+import CreateContact from "@/views/CreateContact.vue"
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import ContactDetails from '@/views/ContactDetails.vue'
+import EditContact from '@/views/EditContact.vue'
+
+
+const routes = [
+  {
+    path: "/",
+    name: 'sign-in',
+    component: LoginPage
+  },
+  {
+    path: "/sign-up",
+    name: "signup",
+    component: SignUpPage
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: ProfilePage,
+
+  },
+  {
+    path: "/sign-up/email/verification",
+    name: "email-verification",
+    component: ""
+  },
+  {
+    path: "/sign-up/email/verify",
+    name: "email-verify",
+    component: EmailVerify
+  },
+  {
+    path: "/contacts",
+    name: "Contacts",
+    component: ContactList
+  },
+  {
+    path: "/contact/create",
+    name: "create-contact",
+    component: CreateContact
+  }, 
+  {
+    path: "/contact/edit/:id",
+    name: "contact edit",
+    component: EditContact
+  },
+  {
+    path: "/contact/details/:id",
+    name: "contact-details",
+    component: ContactDetails
+  }
+]
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
+  history: createWebHistory(),
+  routes
 })
-
 export default router
